6d3613b7ed4ee9cfc90a849e60a04e049f89a964
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-09 23:00:27 +0700

    komentar

---
+++ main.py
@@ -0,0 +1,9 @@
+nama = input("masukkan nama anda :") 
+gaji = int(input("masukkan gaji anda :"))
+
+if gaji > 500 :
+    print("hebat")
+elif gaji < 600 :
+    print("biasa")
+else:
+    print("masih kurang baik")
